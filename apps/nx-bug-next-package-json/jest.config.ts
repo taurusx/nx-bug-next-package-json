c69633add4f566ec0a3c4da52ae0168830d678e6
Sławomir Zawistowski <example@email.example>
2024-02-02 13:41:37 +0100

    Initial commit

---
+++ apps/nx-bug-next-package-json/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'nx-bug-next-package-json',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/nx-bug-next-package-json',
+};
